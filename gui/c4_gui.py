c266718cac27e720cd3210725246a6b59083ad56
yuvasaro <yuvanand.s@gmail.com>
2023-12-19 22:28:54 -0800

    Fixed bug with gui

--- gui/c4_gui.py
+++ gui/c4_gui.py
@@ -59,109 +59,109 @@ class C4_GUI:
                 if event.type == pygame.QUIT:
                     running = False
 
-                self.window.fill(self.black)
+            self.window.fill(self.black)
 
-                for i in range(N):
-                    for j in range(M):
-                        pygame.draw.rect(
-                            self.window, 
-                            self.blue, 
-                            (
-                                i * self.slot_size, 
-                                (j + 1) * self.slot_size, 
-                                self.slot_size,
-                                self.slot_size
-                            )
+            for i in range(N):
+                for j in range(M):
+                    pygame.draw.rect(
+                        self.window, 
+                        self.blue, 
+                        (
+                            i * self.slot_size, 
+                            (j + 1) * self.slot_size, 
+                            self.slot_size,
+                            self.slot_size
                         )
+                    )
 
-                        color = self.black
-                        flip_j = M - j - 1
-                        if self.board.get_coin((flip_j, i)) == P1:
-                            color = self.red
-                        elif self.board.get_coin((flip_j, i)) == P2:
-                            color = self.yellow
+                    color = self.black
+                    flip_j = M - j - 1
+                    if self.board.get_coin((flip_j, i)) == P1:
+                        color = self.red
+                    elif self.board.get_coin((flip_j, i)) == P2:
+                        color = self.yellow
 
-                        pygame.draw.circle(
-                            self.window,
-                            color,
-                            (
-                                i * self.slot_size + self.slot_size / 2,
-                                (j + 1) * self.slot_size + self.slot_size / 2
-                            ),
-                            self.circle_radius
-                        )
-                
-                if self.game_state == ONGOING:
-                    if play_ai and self.game.turn == self.ai:
-                        if self.ai == P1:
-                            q_values = self.ai_p1(self.board.array().reshape(INPUT_SHAPE)).numpy()
+                    pygame.draw.circle(
+                        self.window,
+                        color,
+                        (
+                            i * self.slot_size + self.slot_size / 2,
+                            (j + 1) * self.slot_size + self.slot_size / 2
+                        ),
+                        self.circle_radius
+                    )
+            
+            if self.game_state == ONGOING:
+                if play_ai and self.game.turn == self.ai:
+                    if self.ai == P1:
+                        q_values = self.ai_p1(self.board.array().reshape(INPUT_SHAPE)).numpy()
+                    else:
+                        q_values = self.ai_p2(self.board.array().reshape(INPUT_SHAPE)).numpy()
+
+                    full_cols = self.board.get_full_cols()
+                    np.put(q_values, full_cols, [-math.inf for _ in full_cols])
+                    ai_move = np.argmax(q_values)
+                    self.game.move(self.ai, ai_move)
+                    self.game_state = self.game.check_game_end()
+                    self.game.toggle_turn()
+                    # ai_predict = True
+                else:
+                    # if ai_predict:
+                    #     if self.player == P1:
+                    #         q_values = self.ai_p1(self.board.array().reshape(INPUT_SHAPE)).numpy()
+                    #     else:
+                    #         q_values = self.ai_p2(self.board.array().reshape(INPUT_SHAPE)).numpy()
+                    #     full_cols = self.board.get_full_cols()
+                    #     np.put(q_values, full_cols, [-math.inf for _ in full_cols])
+                    #     ai_move = np.argmax(q_values)
+                    #     print(f"AI thinks your best move is column {ai_move + 1}")
+                    #     ai_predict = False
+
+                    # Draw coin near cursor
+                    if event.type == pygame.MOUSEMOTION:
+                        mouse_x = event.pos[0]
+                        if self.game.turn == P1:
+                            pygame.draw.circle(self.window, self.red, (mouse_x, self.slot_size / 2), self.circle_radius)
                         else:
-                            q_values = self.ai_p2(self.board.array().reshape(INPUT_SHAPE)).numpy()
+                            pygame.draw.circle(self.window, self.yellow, (mouse_x, self.slot_size / 2), self.circle_radius)
+                    
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        self.can_place = True
 
-                        full_cols = self.board.get_full_cols()
-                        np.put(q_values, full_cols, [-math.inf for _ in full_cols])
-                        ai_move = np.argmax(q_values)
-                        self.game.move(self.ai, ai_move)
+                    # Place coin in closest column to cursor
+                    if event.type == pygame.MOUSEBUTTONDOWN and self.can_place:
+                        mouse_x = event.pos[0]
+                        col = int(math.floor(mouse_x / self.slot_size))
+                        self.game.move(self.game.turn, col)
+                        self.can_place = False
+
                         self.game_state = self.game.check_game_end()
                         self.game.toggle_turn()
-                        # ai_predict = True
-                    else:
-                        # if ai_predict:
-                        #     if self.player == P1:
-                        #         q_values = self.ai_p1(self.board.array().reshape(INPUT_SHAPE)).numpy()
-                        #     else:
-                        #         q_values = self.ai_p2(self.board.array().reshape(INPUT_SHAPE)).numpy()
-                        #     full_cols = self.board.get_full_cols()
-                        #     np.put(q_values, full_cols, [-math.inf for _ in full_cols])
-                        #     ai_move = np.argmax(q_values)
-                        #     print(f"AI thinks your best move is column {ai_move + 1}")
-                        #     ai_predict = False
-
-                        # Draw coin near cursor
-                        if event.type == pygame.MOUSEMOTION:
-                            mouse_x = event.pos[0]
-                            if self.game.turn == P1:
-                                pygame.draw.circle(self.window, self.red, (mouse_x, self.slot_size / 2), self.circle_radius)
-                            else:
-                                pygame.draw.circle(self.window, self.yellow, (mouse_x, self.slot_size / 2), self.circle_radius)
-                        
-                        if event.type == pygame.MOUSEBUTTONUP:
-                            self.can_place = True
-
-                        # Place coin in closest column to cursor
-                        if event.type == pygame.MOUSEBUTTONDOWN and self.can_place:
-                            mouse_x = event.pos[0]
-                            col = int(math.floor(mouse_x / self.slot_size))
-                            self.game.move(self.game.turn, col)
-                            self.can_place = False
-
-                            self.game_state = self.game.check_game_end()
-                            self.game.toggle_turn()
+            else:
+                if self.game_state == P1:
+                    text = f"Player {self.game_state} wins!"
+                    text_color = self.red
+                elif self.game_state == P2:
+                    text = f"Player {self.game_state} wins!"
+                    text_color = self.yellow
                 else:
-                    if self.game_state == P1:
-                        text = f"Player {self.game_state} wins!"
-                        text_color = self.red
-                    elif self.game_state == P2:
-                        text = f"Player {self.game_state} wins!"
-                        text_color = self.yellow
-                    else:
-                        text = "It's a draw!"
-                        text_color = self.blue
+                    text = "It's a draw!"
+                    text_color = self.blue
 
-                    text_render = self.font.render(text, False, text_color)
-                    self.window.blit(text_render, (1.7 * self.slot_size, self.slot_size / 12))
+                text_render = self.font.render(text, False, text_color)
+                self.window.blit(text_render, (1.7 * self.slot_size, self.slot_size / 12))
 
-                    if event.type == pygame.KEYDOWN:
-                        # Start a new game if the enter key is pressed
-                        if pygame.key.get_pressed()[pygame.K_RETURN]:
-                            if self.player == P1:
-                                self.player = P2
-                                self.ai = P1
-                            else:
-                                self.player = P1
-                                self.ai = P2
-                            self.game.reset(P1)
-                            self.game_state = ONGOING
+                if event.type == pygame.KEYDOWN:
+                    # Start a new game if the enter key is pressed
+                    if pygame.key.get_pressed()[pygame.K_RETURN]:
+                        if self.player == P1:
+                            self.player = P2
+                            self.ai = P1
+                        else:
+                            self.player = P1
+                            self.ai = P2
+                        self.game.reset(P1)
+                        self.game_state = ONGOING
 
             # Update display
             pygame.display.flip()
